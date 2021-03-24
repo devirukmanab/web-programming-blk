39f948ecee5b3654c73ce2a34821296c2bc410c6
Devi Rukmana B <67756819+devirukmanab@users.noreply.github.com>
2021-03-24 11:10:34 +0700

    update

--- BelajarPHP/BelajarOOP/myClass.php
+++ BelajarPHP/BelajarOOP/myClass.php
@@ -1,16 +1,44 @@
 <?php
-    class PesawatTerbang {
-        public $warnaPesawat;
-
-        public function gantiWarna($warnaBaru) {
-            $this->warnaPesawat = $warnaBaru;
-            return $this-> warnaPesawat;
-        }
-        public function setWarna($warnaBaru) {
-            $this->warnaPesawat = $warnaBaru;
-        }
-        public function getWarna() {
-            return $this->warnaPesawat;
-        }
-    }
+    require_once "myClass.php";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // Pembuatan Objek Warna Pesawat
+        $pesawatA = new PesawatTerbang("Abu-Abu");
+
+        echo "Warna pesawat di awal adalah: ".$pesawatA->getWarna();
+        echo "<br>";
+
+        // Memberikan Nilai
+        $pesawatA->setWarna("Merah");
+
+        // Menampilkan Objek dengan nilai
+        echo "Warna pesawat A adalah: ".$pesawatA->getWarna();
+        echo "<hr>";
+
+
+        // Pembuatan Objek Warna dan Kecepatan Mobil
+        $mobilA = new Mobil("Merah");
+        echo "Warna Mobil A adalah: ".$mobilA->getWarna();
+        echo "<br>";
+        echo "Kecepatan Mobil A adalah: ".$mobilA->getKecepatan();
+        $mobilA->tambahKecepatan(20);
+        echo "<br>";
+        echo "Kecepatan Mobil A adalah: ".$mobilA->getKecepatan();
+        $mobilA->tambahKecepatan(30);
+        echo "<br>";
+        echo "Kecepatan Mobil A adalah: ".$mobilA->getKecepatan();
+
+
+    ?>    
+</body>
+</html>
