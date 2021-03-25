d6836af012ce6f87c184a503a16d72588476a7e9
Devi Rukmana B <67756819+devirukmanab@users.noreply.github.com>
2021-03-25 14:57:58 +0700

    Update Halaman_buku_tamu.php

--- latihanDatabasePHP/Halaman_buku_tamu.php
+++ latihanDatabasePHP/Halaman_buku_tamu.php
@@ -39,7 +39,7 @@
                     </div>
 
                     <div class="form-group">
-                    <input type="submit" class="btn btn-info btn-block" value="Update">
+                    <input type="submit" class="btn btn-info btn-block" value="Kirim">
                     </div>
                 </form> 
     
