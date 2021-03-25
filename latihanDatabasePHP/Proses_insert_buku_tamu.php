ceff5b9921bf3e97c9c60c0792c25aff89ddbcd5
Devi Rukmana B <67756819+devirukmanab@users.noreply.github.com>
2021-03-25 11:57:10 +0700

    Update menambahkan alert Pop up

--- latihanDatabasePHP/Proses_insert_buku_tamu.php
+++ latihanDatabasePHP/Proses_insert_buku_tamu.php
@@ -3,14 +3,29 @@
 
     $nama = $_POST['nama'];
     $email = $_POST['email'];
-    $pesan = $_POST['password']
+    $pesan = $_POST['pesan'];
+
     // Perintah SQL
     $sql = "INSERT INTO tb_tamu VALUES('', '$nama', '$email', '$pesan')";
 
     // Eksekusi Perintah
     if($conn->query($sql) === true) {
+        // header("location:Halaman_buku_tamu.php");
+
+        // Menampilkan Pop Up BERHASIL TERSIMPAN
+        echo "<script>
+                alert('Berhasil tersimpan');
+                location.assign('Halaman_buku_tamu.php');   
+            </script>";
+
         echo "Berhasil tersimpan";
     } else {
+        // Menampilkan Pop Up GAGAL TERSIMPAN
+        echo "<script>
+                alert('Berhasil tersimpan');
+                location.assign('Halaman_buku_tamu.php');   
+            </script>";
+
         echo "Gagal tersimpan";
     }
 
