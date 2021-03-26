2439f67dd1c4581d7beb8738bb0f328477be6e25
Devi Rukmana B <67756819+devirukmanab@users.noreply.github.com>
2021-03-26 14:25:57 +0700

    Update Start Session

--- latihanDatabasePHP/Proses_delete_buku_tamu.php
+++ latihanDatabasePHP/Proses_delete_buku_tamu.php
@@ -2,30 +2,38 @@
     require_once "MySQL_connection.php";
 
   $id = $_GET['idTamu'];
+
+   // Start Session
+   session_start();
   
     // Perintah SQL
     $sql = "DELETE FROM tb_tamu WHERE id_tamu='$id'";
 
     // Eksekusi Perintah
     if($conn->query($sql) === true) {
-        // header("location:Halaman_buku_tamu.php");
-
-        // Menampilkan Pop Up BERHASIL TERHAPUS
-        echo "<script>
-                alert('Berhasil terhapus');
-                location.assign('Halaman_buku_tamu.php');   
-            </script>";
-
-        echo "Berhasil terhapus";
+        $_SESSION['update_status'] = 1; //nilai "1"-nya boleh berapa aja
+        $_SESSION['update_message'] = '<strong>Berhasil!</strong> Data berhasil dihapus!';
+        header("location:Halaman_buku_tamu.php");
     } else {
-        // Menampilkan Pop Up GAGAL TERHAPUS
-        echo "<script>
-                alert('Berhasil terhapus');
-                location.assign('Halaman_buku_tamu.php');   
-            </script>";
+        $_SESSION['update_status'] = 1; //nilai "1"-nya boleh berapa aja
+        $_SESSION['update_message'] = '<strong>Gagal!</strong> Data gagal dihapus!';
+    }    
+        // Menampilkan Pop Up BERHASIL TERHAPUS
+    //     echo "<script>
+    //             alert('Berhasil terhapus');
+    //             location.assign('Halaman_buku_tamu.php');   
+    //         </script>";
 
-        echo "Gagal terhapus";
-    }
+    //     echo "Berhasil terhapus";
+    // } else {
+    //     // Menampilkan Pop Up GAGAL TERHAPUS
+    //     echo "<script>
+    //             alert('Berhasil terhapus');
+    //             location.assign('Halaman_buku_tamu.php');   
+    //         </script>";
+
+    //     echo "Gagal terhapus";
+    // }
 
 ?>
 
